b5afd0cc70dda74d7118eea41cb9c66b54196c20
mihkuno <caindayjoeninyo@gmail.com>
2024-06-27 18:05:16 +0800

    added template test for get requests

--- server/test/test_getmeth.js
+++ server/test/test_getmeth.js
@@ -1,7 +1,28 @@
-import Database from "../lib/database";
+import Database from "../lib/database.js";
 
 // Database is an object that contains the following properties:
 
-// Database.Classroom
 // Database.Student
 // Database.Schedule
+
+// Connect to the database
+Database.mongoose
+    .connect(Database.url, {})
+    .then(async () => {
+        console.log("Connected to MongoDataBase");
+        console.log(process.env.MONGOURL);
+
+        // TODO: Create the get function here..
+        // function getStudentName(studentId) {}
+        // function getStudentSchedule(studentId) {}
+        // function getStudentClassmates(studentId) {}
+
+        // TODO: Test the get function here..
+        // getStudentName("2022309477")
+    })
+    .catch((err) => {
+        console.log(err);
+});
+
+
+
